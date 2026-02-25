facb4fd9cf405b4cdf4796e1105b0d625292c393
nddsv25 <dattinsv25@gmail.com>
2026-02-25 14:52:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function selectPath(choice) {
+    if (choice === 'beauty') {
+        window.location.href = "beauty.html";
+    } else if (choice === 'fitness') {
+        window.location.href = "fitness.html";
+    }
+}
